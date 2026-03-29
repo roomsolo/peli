4ca3804f2df9c28f77c9c523ad2fa075099a7167
roomsolo <brky.clk0@gmail.com>
2026-03-29 22:49:11 +0300

    initial.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// main.rs
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
